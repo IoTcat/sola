3900393d92bee728b9dac72b11b7d06b70a0f407
iotcat <i@iotcat.me>
2019-08-10 14:37:35 +0800

    docs

---
+++ docs/README.md
@@ -0,0 +1,44 @@
+# Sola Proj
+
+Sola的意思是天空，被太阳照射的地方。祝愿Sola Proj能够成为梦想与希望的起源之地。
+
+## 重要网址
+
+**主页**
+[https://home.yimian.xyz](https://home.yimian.xyz)
+
+**FTP家庭网盘**
+[ftps://home.yimian.xyz:2121](ftps://home.yimian.xyz:2121)
+
+**项目博客**
+[https://www.eee.dog/tech/smart-home.html](https://www.eee.dog/tech/smart-home.html)
+
+## 系统组成
+
+### 基本组成
+
+````
+|
+|---网络组件
+|   |---主路由器
+|   |---辅路由器
+|   |---LoRa网关
+|
+|
+|---计算单元
+|   |---单片机节点
+|   |---IBM服务器
+|   |---云服务器
+|
+|
+|---公共终端
+|   |---天猫精灵
+|   |---MING's笔记本
+|   |---XIUJU's笔记本
+|   |---MING's IPhone
+|   |---XIUJU's IPhone
+|
+
+
+````
+
---
+++ docs/config.js
@@ -0,0 +1,47 @@
+const langs = [
+    { title: '简体中文', path: '/home', matchPath: /^\/(home|ecosystem|support)/ },
+];
+
+docute.init({
+    landing: 'landing.html',
+    title: 'Sola Proj',
+    repo: 'iotcat/sola',
+    twitter: 'iotcat',
+    'edit-link': 'https://github.com/IoTcat/sola/blob/master/docs/',
+    nav: {
+        default: [
+            {
+                title: '首页', path: '/en/'
+            }
+        ],
+    },
+    plugins: [
+        docsearch({
+            apiKey: '46a8ba306e368972c1d75feb581fb430',
+            indexName: 'wiot',
+            tags: ['english', 'zh-Hans'],
+            url: 'http://wiot.eee.dog'
+        }),
+        evanyou()
+    ]
+});
+
+function loadScript (src, callback) {
+    let s,
+        r,
+        t;
+    r = false;
+    s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.src = src;
+    s.onload = s.onreadystatechange = function () {
+    // console.log( this.readyState ); //uncomment this line to see which ready states are called.
+        if (!r && (!this.readyState || this.readyState == 'complete'))
+        {
+            r = true;
+            callback();
+        }
+    };
+    t = document.getElementsByTagName('script')[0];
+    t.parentNode.insertBefore(s, t);
+}
